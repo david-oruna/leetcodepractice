f553119c01e5bceebbf087e29ab36671b0082f66
David Hawk <104524650+david-oruna@users.noreply.github.com>
2022-09-24 12:37:52 -0500

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def fizzbuzz(x):
+        for i in range(x+1):
+            if i == 0:
+                pass
+            else:
+                if i % 3 == 0:
+                    if i % 5 == 0:
+                        print('FizzBuzz')
+                    else:
+                        print('Fizz')
+                elif i % 5 == 0:
+                    print('Buzz')
+                
+                else:
+                    print(i)
+
+    fizzbuzz(15)
