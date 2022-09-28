f7c4a9cd447d3c91449ea225250ca22c993c9e58
David Hawk <104524650+david-oruna@users.noreply.github.com>
2022-09-27 22:09:43 -0500

    Create Middle of the Linked List.py

---
+++ Middle of the Linked List.py
@@ -0,0 +1,20 @@
+class Solution:
+    def middleNode(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+      
+
+
+      
+ class Solution:
+    def middleNode(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow      
+      
+ # The next() function returns the next item from the iterator.
